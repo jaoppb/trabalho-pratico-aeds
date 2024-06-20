5c51c8087b7dd3b19ea58a54c7ed0b0a51adde42
jao <joaopedroperes06@gmail.com>
2024-06-19 23:23:36 -0300

    implementação da Classe Quartos para gerenciar os quartos

--- src/quarto.hpp
+++ src/quarto.hpp
@@ -1,6 +1,8 @@
 #ifndef QUARTO_HPP
 #define QUARTO_HPP
 
+#include <vector>
+
 class Quarto {
 private:
 	int numero;
@@ -11,14 +13,26 @@ private:
 public:
 	Quarto(int numero, float diaria);
 
-	int getNumero() const;
-	int getQuantidadeDeHospedes() const;
+	int   getNumero() const;
+	int   getQuantidadeDeHospedes() const;
 	float getDiaria() const;
-	bool getStatus() const;
+	bool  getStatus() const;
 
 	void setQuantidadeDeHospedes(int hospedes);
 	void setDiaria(float diaria);
 	void setStatus(bool status);
+};
+
+class Quartos {
+private:
+	std::vector<Quarto*> quartos;
+public:
+	Quartos();
+	~Quartos();
+	const Quarto* criarQuarto(int numero, float valorDiaria);
+	const Quarto* getQuarto  (int numero) const;
+	void          setStatus  (int numero, bool status);
+	const bool	  getStatus  (int numero) const;
 };
 
 #endif
