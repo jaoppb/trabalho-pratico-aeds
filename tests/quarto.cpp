e78a22a02556e47d8295e293c76b8340a463c5dd
jao <joaopedroperes06@gmail.com>
2024-06-19 23:32:56 -0300

    testes básicos da Classe Quartos

--- tests/quarto.cpp
+++ tests/quarto.cpp
@@ -30,3 +30,39 @@ TEST_CASE("Exceptions da Classe Quarto") {
 	CHECK_THROWS( Quarto( 100,   0.0f) );
 	CHECK_THROWS( Quarto( -20, -24.0f) );
 }
+
+TEST_CASE("Testa básico da Classe Quartos") {
+	{
+		Quartos quartos;
+		const Quarto *quarto10 = quartos.criarQuarto(10, 24.0f);
+		
+		CHECK(quarto10 -> getNumero() == 10);
+		CHECK(quarto10 -> getDiaria() == 24.0f);
+		
+		CHECK_THROWS(quartos.criarQuarto(10, 24.51f));
+		CHECK_THROWS(quartos.criarQuarto(20, 0.0f));
+		CHECK_THROWS(quartos.criarQuarto(15, -10.0f));
+		CHECK_THROWS(quartos.criarQuarto(-4, 18.0f));
+		CHECK_THROWS(quartos.criarQuarto(-8, -1.0f));
+
+		CHECK(quartos.getStatus(10) == false);
+		quartos.setStatus(10, true);
+		CHECK(quartos.getStatus(10) == true);
+
+		CHECK_THROWS(quartos.getStatus(20));
+		CHECK_THROWS(quartos.getStatus(15));
+		CHECK_THROWS(quartos.setStatus(20, false));
+		CHECK_THROWS(quartos.setStatus(15, true));
+
+		const Quarto *quarto05, *quarto20;
+		CHECK_NOTHROW(quarto05 = quartos.criarQuarto( 5, 19.9f));
+		CHECK_NOTHROW(quarto20 = quartos.criarQuarto(20, 14.9f));
+
+		CHECK(quartos.getQuarto( 5) == quarto05);
+		CHECK(quartos.getQuarto(10) == quarto10);
+		CHECK(quartos.getQuarto(20) == quarto20);
+
+		CHECK_THROWS(quartos.getQuarto( 15));
+		CHECK_THROWS(quartos.getQuarto(-24));
+	}
+}
