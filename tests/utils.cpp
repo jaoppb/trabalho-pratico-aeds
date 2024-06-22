0bbc79cc75e79123141eb8c5326918800db0ffc3
jao <joaopedroperes06@gmail.com>
2024-06-22 11:19:27 -0300

    adição de testes para o parseDate

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -13,11 +13,20 @@ TEST_CASE("Teste básico do parseDate") {
 		CHECK(parseDate("29/02/2024 - 12:00:00") == 1709218800);
 	}
 	{
+		CHECK(parseDate("01/01/2024") == 1704078000);
+		CHECK(parseDate("21/06/2024") == 1718938800);
+	}
+	{
 		CHECK_THROWS(parseDate("32/13/2024 - 25:61:61"));
 		CHECK_THROWS(parseDate("2024/06/21 - 15:30:00"));
 		CHECK_THROWS(parseDate("2024/06/21 - 15:30:00"));
 		CHECK_THROWS(parseDate("31/02/2024 - 12:00:00"));
 		CHECK_THROWS(parseDate("29/02/2023 - 12:00:00"));
 		CHECK_THROWS(parseDate("31/04/2024 - 10:00:00"));
+	}
+	{
+		CHECK_THROWS(parseDate("31/02/2024"));
+		CHECK_THROWS(parseDate("29/02/2023"));
+		CHECK_THROWS(parseDate("31/04/2024"));
 	}
 }
