36346d6086c168e3871e073f8ff005104a6e2187
jao <joaopedroperes06@gmail.com>
2024-06-21 21:06:52 -0300

    padronização da classes cliente e cadastroCliente

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -6,68 +6,99 @@ using namespace std;
 
 // Implementação dos métodos da classe Cliente
 
-int Cliente::getCodigo() const {
+Cliente::Cliente(
+    unsigned int codigo,
+    unsigned int telefone,
+    string endereco,
+    string nome
+): codigo(codigo), tel(telefone), endereco(endereco), nome(nome) {}
+
+const unsigned int Cliente::getCodigo() const {
     return codigo;
 }
 
-void Cliente::setCodigo(int codigo) {
-    this->codigo = codigo;
+const unsigned int Cliente::getTelefone() const {
+    return tel;
+}
+
+const string Cliente::getEndereco() const {
+    return endereco;
+}
+
+const string Cliente::getNome() const {
+    return nome;
 }
 
 void Cliente::setTelefone(int tel) {
     this->tel = tel;
 }
 
-int Cliente::getTelefone() {
-    return tel;
-}
-
 void Cliente::setEndereco(string casa) {
     this->endereco = casa;
-}
-
-string Cliente::getEndereco() {
-    return endereco;
 }
 
 void Cliente::setNome(string name) {
     this->nome = name;
 }
 
-string Cliente::getNome() {
-    return nome;
+// Implementação dos métodos da classe Clientes
+
+Clientes::Clientes(): clientes({}), proximoCodigo(0) {}
+
+Clientes::~Clientes() {
+    for(Cliente *cliente: this -> clientes) {
+        delete cliente;
+    }
 }
 
-// Implementação dos métodos da classe CadastroCliente
-
-int CadastroCliente::gerarCodigo() {
-    static int ultimoCodigo = 0; // mantem o ultimo código gerado
-    return ++ultimoCodigo; // incrementa e retorna o último código
+const int Clientes::gerarCodigo() {
+    return ++proximoCodigo; // incrementa e retorna o próximo código
 }
 
-bool CadastroCliente::checkCodigo(int codigo) {
-    int VectorH = clientes.size();
-    for(int i = 0; i < VectorH; i++) { // auto permite o compilador deduzir automaticamente o tipo de elemento, esse for percorre todos os elementos do vetor cliente   
-        if(clientes[i].getCodigo() == codigo) {
+bool Clientes::checkCodigo(int codigo) {
+    for(Cliente *cliente: this -> clientes) { // percorre todos os valores de clientes e a cada valor roda o for atribuindo o valor a cliente 
+        if(cliente -> getCodigo() == codigo) {
             return true;
         }
     }
     return false; // essa função serve para verificar se já existe um cliente cadastrado com certo código
 }
 
-void CadastroCliente::addCliente(Cliente cliente) {
-    if(checkCodigo(cliente.getCodigo())) {
+const Cliente* Clientes::addCliente(
+    unsigned int telefone,
+    string endereco,
+    string nome
+) {
+    unsigned int code = this -> gerarCodigo();
+    if(this -> checkCodigo(code)) {
         cout << "Código já cadastrado." << endl; // codigo ja cadastrado
-    } else {
-        cliente.setCodigo(gerarCodigo()); // gera um novo código para o cliente
-        clientes.push_back(cliente); // adiciona o código gerado no vetor cliente
+        return nullptr; //retorna NULL como não foi possível cadastrar o cliente
     }
+
+    Cliente *cliente = new Cliente(
+        code,
+        telefone,
+        endereco,
+        nome
+    ); //gera um pointeiro para o objeto cliente
+    
+    clientes.push_back(cliente); // adiciona o código gerado no vetor cliente
+    return cliente;
 }
 
-bool CadastroCliente::removerCliente(int codigo) {
-    int VectorH = clientes.size();
-    for(int i = 0; i < VectorH; i++) {
-        if(clientes[i].getCodigo() == codigo) {
+const Cliente* Clientes::getCliente(unsigned int codigo) {
+    for(Cliente *cliente: this -> clientes) {
+        if(cliente -> getCodigo() == codigo) {
+            return cliente;
+        }
+    }
+    return nullptr;
+}
+
+const bool Clientes::removerCliente(unsigned int codigo) {
+    int len = clientes.size(); // len = length = largura/tamanho
+    for(int i = 0; i < len; i++) {
+        if(this -> clientes[i] -> getCodigo() == codigo) {
             clientes.erase(clientes.begin() + i); // erase serve para remover elementos 
             return true;
         }
--- src/cliente.hpp
+++ src/cliente.hpp
@@ -8,29 +8,43 @@ using namespace std;
 
 class Cliente {
 private:
-    int codigo, tel;
+    unsigned int codigo, tel; //unsigned = não guarda o sinal do número ou seja so armazena 0 ou positivo
     string endereco, nome;
 
 public:
-    int getCodigo() const; 
-    void setCodigo(int codigo);
+    Cliente(
+        unsigned int codigo,
+        unsigned int telefone,
+        string endereco,
+        string nome
+    );
+
+    const unsigned int getCodigo() const; //const no tipo impede alteração depois de pegar o valor 
+    const unsigned int getTelefone() const; //const depois do metódo indica que não altera nenhum valor do objeto
+    const string getEndereco() const;
+    const string getNome() const;
+
     void setTelefone(int tel);
-    int getTelefone(); 
     void setEndereco(string casa);
-    string getEndereco(); 
     void setNome(string name);
-    string getNome(); 
 };
 
-class CadastroCliente {
+class Clientes {
 private:
-    vector<Cliente> clientes;
-    int gerarCodigo();
-
+    vector<Cliente*> clientes; //usar ponteiro para armazenar valores permite que a memória fique mais distribuída
+    unsigned int proximoCodigo;
+    const int gerarCodigo();
+    bool checkCodigo(int codigo); //como cliente vai ser gerado dentro da classe não precisa de ser público
 public:
-    void addCliente(Cliente cliente);
-    bool removerCliente(int codigo);
-    bool checkCodigo(int codigo);
+    Clientes(); //alguns valores precisam ser inicializados
+    ~Clientes(); //como os clientes estão sendo gerados com `new` é uma boa prático limpá-los
+    const Cliente* addCliente( //retorna o ponteiro com acesso somente a leitura depois de criado
+        unsigned int telefone,
+        string endereco,
+        string nome
+    );
+    const Cliente *getCliente(unsigned int codigo);
+    const bool removerCliente(unsigned int codigo);
 };
 
 #endif
