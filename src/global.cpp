7e2aca9365fe3e62fabf2c97a9ab38e80d955d58
jao <joaopedroperes06@gmail.com>
2024-06-22 21:35:28 -0300

    adição da função que retorna o timestamp do momento em que ela foi chamada

--- src/global.cpp
+++ src/global.cpp
@@ -3,3 +3,5 @@
 Quartos  *quartosHandler  = new Quartos();
 Estadias *estadiasHandler = new Estadias();
 Pessoas  *pessoasHandler  = new Pessoas();
+
+time_t getCurrentTimestamp() { return difftime(std::time(0), timezone); }
