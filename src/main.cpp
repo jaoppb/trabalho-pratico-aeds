920050e723202fee326393ab9ab804cc4e734996
jao <joaopedroperes06@gmail.com>
2024-06-25 05:48:10 -0300

    pequenas correções/alterações ao menu e adição de try catch

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,24 @@
 
 #include "global.hpp"
 
-void Menu()
-{
-	bool clienteCadastrado = false;
-	bool quartoCadastrado = false;
+void menu() {
+	bool clienteCadastrado = pessoasHandler->getTotalClients() > 0;
+	bool quartoCadastrado = quartosHandler->getTotal() > 0;
 
 	int result = 0;
-	while (result != 6)
-	{
-
+	while (result != 6) {
 		std::cout << "Escolha a opção desejada conforme indicado no menu a seguir: " << std::endl
-				  << "1 - Cadastrar um cliente.\n2 - Cadastrar um quarto.\n3 - Cadastrar um funcionário.\n4 - Cadastrar uma estadia.\n5 - Pesquisar um cliente/funcionário\n6 - Sair do menu." << std::endl;
+				  << "1 - Cadastrar um cliente.\n"
+				  << "2 - Cadastrar um quarto.\n"
+				  << "3 - Cadastrar um funcionário.\n"
+				  << "4 - Cadastrar uma estadia.\n"
+				  << "5 - Pesquisar um cliente/funcionário\n"
+				  << "6 - Sair do menu." << std::endl;
 		std::cin >> result;
 		std::cin.ignore();
 
-		switch (result)
-		{
-		case 1:
-		{
+		switch (result) {
+		case 1: {
 			std::string name = "";
 			std::string address = "";
 			unsigned int cellPhone = 0;
@@ -36,33 +36,39 @@ void Menu()
 			std::cout << "Informe seu número de telefone: " << std::endl;
 			std::cin >> cellPhone;
 
-			const Cliente *cliente = pessoasHandler->cadastrarCliente(cellPhone, name, address, points);
-			cliente->getCodigo();
+			try {
+				const Cliente *cliente = pessoasHandler->cadastrarCliente(cellPhone, name, address, points);
+				cliente->getCodigo();
 
-			std::cout << std::endl;
+				std::cout << std::endl;
 
-			std::cout << "Cliente cadastrado com sucesso! Seja bem vindo(a) Sr(a). " << name << " ao Hotel Descanso Garantido, aqui o seu descanso é garantido ou seu dinheiro de volta. O seu código de cadastro é : " << cliente->getCodigo() << std::endl;
+				std::cout << "Cliente cadastrado com sucesso! Seja bem vindo(a) Sr(a). " << name << " ao Hotel Descanso Garantido, aqui o seu descanso é garantido ou seu dinheiro de volta. O seu código de cadastro é : " << cliente->getCodigo() << std::endl;
 
-			clienteCadastrado = true;
+				clienteCadastrado = true;
+			} catch(std::runtime_error &err) {
+				std::cout << err.what() << std::endl;
+			}
 
 			break;
 		}
-		case 2:
-		{
+		case 2: {
 			unsigned int number = 0;
 			float dailyValue = 750.0;
 
 			std::cout << "Para todas as diárias, o valor é de R$750,00. Informe o número do quarto desejado: " << std::endl;
 			std::cin >> number;
 
-			quartosHandler->criarQuarto(number, dailyValue);
+			try {
+				quartosHandler->criarQuarto(number, dailyValue);
 
-			quartoCadastrado = true;
+				quartoCadastrado = true;
+			} catch(std::runtime_error &err) {
+				std::cout << err.what() << std::endl;
+			}
 
 			break;
 		}
-		case 3:
-		{
+		case 3: {
 			unsigned int cellPhone = 0;
 			std::string name = "";
 			std::string cargo = "";
@@ -80,16 +86,18 @@ void Menu()
 			std::cout << "Informe o número de telefone do funcionário " << std::endl;
 			std::cin >> cellPhone;
 
-			pessoasHandler->cadastrarFuncionaro(cellPhone, name, cargo, wage);
+			try {
+				pessoasHandler->cadastrarFuncionaro(cellPhone, name, cargo, wage);
 
-			std::cout << "Funcionário cadastrado com sucesso ! Seja bem vindo Sr(a) " << name << " ao seu novo ambiente de trabalho. " << std::endl;
+				std::cout << "Funcionário cadastrado com sucesso ! Seja bem vindo Sr(a) " << name << " ao seu novo ambiente de trabalho. " << std::endl;			
+			} catch(std::runtime_error &err) {
+				std::cout << err.what();
+			}
 
 			break;
 		}
-		case 4:
-		{
-			if (quartoCadastrado == true && clienteCadastrado == true)
-			{
+		case 4: {
+			if (quartoCadastrado == true && clienteCadastrado == true) {
 				std::string entryDate;
 				std::string exitDate;
 				int clientCode;
@@ -107,92 +115,81 @@ void Menu()
 				std::cout << "Informe o quarto do cliente " << std::endl;
 				std::cin >> numberRoom;
 
-				estadiasHandler->agendarEstadia(entryDate, exitDate, clientCode, numberRoom);
+				try {
+					estadiasHandler->agendarEstadia(entryDate, exitDate, clientCode, numberRoom);
 
-				std::cout << "Estadia cadastrada com sucesso! Aproveite seus dias de descanso no melhor hotel para um bom relaxamento de qualidade da cidade" << std::endl;
+					std::cout << "Estadia cadastrada com sucesso! Aproveite seus dias de descanso no melhor hotel para um bom relaxamento de qualidade da cidade" << std::endl;
+				} catch(std::runtime_error &err) {
+					std::cout << err.what() << std::endl;
+				}
+			} else {
+				std::cout << "Para cadastrar uma estadia primeiro é necessário que um quarto e um cliente estejam cadastrados." << std::endl;
 			}
-			else
-			{
-				std::cout << "Para cadastrar uma estadia primeiro é necessário que o quarto e o cliente estejam cadastrados." << std::endl;
-			}
-
 			break;
 		}
-		case 5:
-		{
+		case 5: {
 			int searchOption = 0;
-			std::cout << "Escolha o método de pesquisa: \n1 - Pesquisar por nome\n2 - Pesquisar por código\n";
-			std::cin >> searchOption;
+			while(searchOption < 1 || searchOption > 3) {
+				std::cout << "Escolha o método de pesquisa: \n"
+						<< "1 - Pesquisar por nome\n"
+						<< "2 - Pesquisar por código\n"
+						<< "3 - Sair das pesquisas\n";
+				std::cin >> searchOption;
+			}
 			std::cin.ignore(); // Limpa o buffer
 
-			if (searchOption == 1)
-			{
+			if (searchOption == 1) {
 				std::string nome;
 				std::cout << "Digite o nome da pessoa que deseja pesquisar: " << std::endl;
 				std::getline(std::cin, nome);
 
-				std::vector<Pessoa *> pessoasEncontradas = pessoasHandler->pesquisarPessoaPorNome(nome);
+				std::vector<Pessoa*> pessoasEncontradas = pessoasHandler->getPessoa(nome);
 
-				if (pessoasEncontradas.empty())
-				{
-					std::cout << "Nenhuma pessoa encontrada com o nome " << nome << "." << std::endl;
-				}
-				else
-				{
-					for (Pessoa *pessoa : pessoasEncontradas)
-					{
+				if (pessoasEncontradas.empty()) {
+					std::cout << "Nenhuma pessoa com o nome " << nome << " encontrada." << std::endl;
+				} else {
+					for (Pessoa *pessoa : pessoasEncontradas) {
 						Cliente *cliente = dynamic_cast<Cliente *>(pessoa);
 						Funcionario *funcionario = dynamic_cast<Funcionario *>(pessoa);
 
-						if (cliente)
-						{
+						if (cliente) {
 							std::cout << "Cliente encontrado: " << std::endl;
-							std::cout << "Código: " << cliente->getCodigo() << std::endl;
-							std::cout << "Nome: " << cliente->getNome() << std::endl;
-							std::cout << "Telefone: " << cliente->getTelefone() << std::endl;
-							std::cout << "Endereço: " << cliente->getEndereco() << std::endl;
-							std::cout << "Pontos: " << cliente->getPontos() << std::endl;
-						}
-						else if (funcionario)
-						{
+							std::cout << " - Código: "   		<< cliente->getCodigo() << std::endl;
+							std::cout << " - Nome: "     		<< cliente->getNome() << std::endl;
+							std::cout << " - Telefone: " 		<< cliente->getTelefone() << std::endl;
+							std::cout << " - Endereço: " 		<< cliente->getEndereco() << std::endl;
+							std::cout << " - Pontos: "   		<< cliente->getPontos() << std::endl;
+						} else if (funcionario) {
 							std::cout << "Funcionário encontrado: " << std::endl;
-							std::cout << "Código: " << funcionario->getCodigo() << std::endl;
-							std::cout << "Nome: " << funcionario->getNome() << std::endl;
-							std::cout << "Telefone: " << funcionario->getTelefone() << std::endl;
-							std::cout << "Cargo: " << funcionario->getCargo() << std::endl;
-							std::cout << "Salário: " << funcionario->getSalario() << std::endl;
+							std::cout << " - Código: "				<< funcionario->getCodigo()   << std::endl;
+							std::cout << " - Nome: "				<< funcionario->getNome() 	  << std::endl;
+							std::cout << " - Telefone: "			<< funcionario->getTelefone() << std::endl;
+							std::cout << " - Cargo: "				<< funcionario->getCargo() 	  << std::endl;
+							std::cout << " - Salário: "				<< funcionario->getSalario()  << std::endl;
 						}
 					}
 				}
-			}
-			else if (searchOption == 2)
-			{
+			} else if (searchOption == 2) {
 				unsigned int codigo;
 				std::cout << "Digite o código da pessoa que deseja pesquisar: " << std::endl;
 				std::cin >> codigo;
 
-				Pessoa *pessoaEncontrada = pessoasHandler->pesquisarPessoaPorCodigo(codigo);
+				const Pessoa *pessoaEncontrada = pessoasHandler->getPessoa(codigo);
 
-				if (!pessoaEncontrada)
-				{
+				if (!pessoaEncontrada) {
 					std::cout << "Nenhuma pessoa encontrada com o código " << codigo << "." << std::endl;
-				}
-				else
-				{
-					Cliente *cliente = dynamic_cast<Cliente *>(pessoaEncontrada);
-					Funcionario *funcionario = dynamic_cast<Funcionario *>(pessoaEncontrada);
+				} else {
+					const Cliente *cliente = dynamic_cast<const Cliente *>(pessoaEncontrada);
+					const Funcionario *funcionario = dynamic_cast<const Funcionario *>(pessoaEncontrada);
 
-					if (cliente)
-					{
+					if (cliente) {
 						std::cout << "Cliente encontrado: " << std::endl;
 						std::cout << "Código: " << cliente->getCodigo() << std::endl;
 						std::cout << "Nome: " << cliente->getNome() << std::endl;
 						std::cout << "Telefone: " << cliente->getTelefone() << std::endl;
 						std::cout << "Endereço: " << cliente->getEndereco() << std::endl;
 						std::cout << "Pontos: " << cliente->getPontos() << std::endl;
-					}
-					else if (funcionario)
-					{
+					} else if (funcionario) {
 						std::cout << "Funcionário encontrado: " << std::endl;
 						std::cout << "Código: " << funcionario->getCodigo() << std::endl;
 						std::cout << "Nome: " << funcionario->getNome() << std::endl;
@@ -201,12 +198,9 @@ void Menu()
 						std::cout << "Salário: " << funcionario->getSalario() << std::endl;
 					}
 				}
+			} else if(searchOption != 3)  {
+				std::cout << "Opção inválida. Voltando ao menu principal.\n";
 			}
-			else
-			{
-				std::cout << "Opção inválida. Tente novamente." << std::endl;
-			}
-
 			break;
 		}
 		case 6:
@@ -216,12 +210,17 @@ void Menu()
 			std::cout << "Opção inválida, digite novamente." << std::endl;
 			break;
 		}
+		if(result != 6) result = 0;
 	}
 }
 
 int main()
 {
-	Menu();
+	menu();
+
+	delete estadiasHandler;
+	delete pessoasHandler;
+	delete quartosHandler;
 
 	return 0;
 }
