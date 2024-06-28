8f14e867e0be505da3f51311ba6bc548f14af555
jao <joaopedroperes06@gmail.com>
2024-06-28 13:54:56 -0300

    testes de Pessoas

--- tests/pessoa.cpp
+++ tests/pessoa.cpp
@@ -14,3 +14,31 @@ TEST_CASE("Testes basicos para a Classe Pessoa") {
     CHECK(pessoa2.getTelefone() == "3198888888");
     CHECK(pessoa2.getNome() == "Ana Maria");
 }
+
+TEST_CASE("Testes para o Handler de Pessoas") {
+    SECTION("Cadastro de Cliente") {
+        const Cliente *cliente1 = nullptr;
+        REQUIRE_NOTHROW(cliente1 = pessoasHandler->cadastrarCliente("12345678", "João Augusto", "R. Um, 200", 0));
+        
+        REQUIRE(pessoasHandler->getCliente(cliente1->getCodigo()) == cliente1);
+        CHECK(cliente1->getTelefone() == "12345678");
+        CHECK(cliente1->getNome() == "João Augusto");
+        CHECK(cliente1->getPontos() == 0);
+
+        REQUIRE_NOTHROW(pessoasHandler->addPontos(cliente1, 5));
+        CHECK(cliente1->getPontos() == 50);
+
+        CHECK_NOTHROW(pessoasHandler->cadastrarCliente("213241214", "Diego Borges", "R. Dois, 200", -213));
+        CHECK_THROWS(pessoasHandler->cadastrarCliente("232141231", "Ana Clara", "", -13));
+    }
+
+    SECTION("Cadastro de Funcionário") {
+        const Funcionario *funcionario1 = nullptr;
+        REQUIRE_NOTHROW(funcionario1 = pessoasHandler->cadastrarFuncionaro("145689624", "Fernando Gomes", "Gerente", 1230.4f));
+
+        REQUIRE(pessoasHandler->getFuncionario(funcionario1->getCodigo()) == funcionario1);
+        CHECK(funcionario1->getTelefone() == "145689624");
+        CHECK(funcionario1->getNome() == "Fernando Gomes");
+        CHECK(funcionario1->getCargo() == "Gerente");
+    }
+}
